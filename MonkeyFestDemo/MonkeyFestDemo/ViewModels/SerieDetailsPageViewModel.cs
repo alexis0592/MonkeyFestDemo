7a46a656b7b8bddd54f459f565c3e70a5b6f2ee1
yacalder <yacalder@bancolombia.com.co>
2019-09-28 10:40:59 -0500

    adding app center analytics

--- MonkeyFestDemo/MonkeyFestDemo/ViewModels/SerieDetailsPageViewModel.cs
+++ MonkeyFestDemo/MonkeyFestDemo/ViewModels/SerieDetailsPageViewModel.cs
@@ -1,4 +1,7 @@
-﻿using MonkeyFestDemo.Models;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using MonkeyFestDemo.Models;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
@@ -26,10 +29,22 @@ namespace MonkeyFestDemo.ViewModels
         {
             base.Initialize(parameters);
             Serie = (Serie)parameters["model"];
-            
+
             //TODO: Add Analytics to track event with the Serie's data
-            
+            AppCenter.Start("a5cf44a9-a37b-4c72-b4b5-07092d4cf7ec",
+                   typeof(Analytics), typeof(Crashes));
+            Analytics.TrackEvent("List of series fetched from internet");
+            var analyticsData = new Dictionary<string, string> { { "Name", Serie.Name } };
+            Analytics.TrackEvent("Serie clicked", analyticsData);
             //TODO: Add fake crash if the name of the serie contains "Game of"
+
+            AppCenter.Start("a5cf44a9-a37b-4c72-b4b5-07092d4cf7ec",
+                               typeof(Analytics), typeof(Crashes));
+            if (Serie.Name.Contains("Game of"))
+            {
+                Crashes.TrackError(new Exception("The winter is here!"));
+            }
+
         }
 
     }
